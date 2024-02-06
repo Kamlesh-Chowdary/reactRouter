75db96e6ccf71624457bb8df2ee49d64c0d87891
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-02-05 18:15:54 -0800

    layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1 className="bg-red-400 text-center p-10 font-bold text-5xl">
-        Namaste
-      </h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import Layout from "./Layout.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import { Home, About, Contact } from "./components/index.js";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
